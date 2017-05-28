855cdc2ce5636fcd427f345c8e532844a0de5bfc
esteban1234 <tva_libra@hotmail.com>
2017-05-28 12:14:11 -0500

    index listo

--- index.php
+++ index.php
@@ -16,14 +16,6 @@
     </p>
 
 </div>
-<!-- <article class="contenlogo">
-    <figure class="pull-left">
-        <img src="img/muuch.png" alt="MUUCHXIIMBAL" class="img-responsive">
-    </figure>
-    <div class="menu pull-right">
-    	MENU <span><i class="fa fa-bars" aria-hidden="true"></i></span>
-    </div>
-</article> -->
 
 <div id="topo">
   <div class="container">
@@ -55,14 +47,94 @@
   </div><!--Fim da coontainer -->
 </div><!--Fim do topo -->
 
-<article class="banner"></article>
+<article class="banner">
+  <article class="fondo">
+    <p>El paso primordial para asegurar tu inversión, consultoria y tramites de impacto, factibilidad y supervisión ambiental.</p>
+    <!-- <p class="boton"><a class="btn-warning" href="">CONTACTAR</a></p> -->
+    <div class="container0">
+      <a class="btn btn-1">CONTACTAR</a>
+    </div>
+  </article>
+</article>
 
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur sunt eaque molestias corporis possimus dolorum consectetur voluptatem, ut, consequatur ipsam dicta soluta! Nobis dolorum ut ullam velit illum sunt, ipsum!</p> <br><br>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur sunt eaque molestias corporis possimus dolorum consectetur voluptatem, ut, consequatur ipsam dicta soluta! Nobis dolorum ut ullam velit illum sunt, ipsum!</p> <br><br>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur sunt eaque molestias corporis possimus dolorum consectetur voluptatem, ut, consequatur ipsam dicta soluta! Nobis dolorum ut ullam velit illum sunt, ipsum!</p> <br><br>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur sunt eaque molestias corporis possimus dolorum consectetur voluptatem, ut, consequatur ipsam dicta soluta! Nobis dolorum ut ullam velit illum sunt, ipsum!</p> <br><br>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur sunt eaque molestias corporis possimus dolorum consectetur voluptatem, ut, consequatur ipsam dicta soluta! Nobis dolorum ut ullam velit illum sunt, ipsum!</p> <br><br>
+<div class="section" style="margin-top: 2%;">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <img src="img/construc.jpg"
+            class="img-responsive">
+          </div>
+          <div class="col-md-6 texto">
+            <h1 style="color: rgb(58,58,58);">¿PORQUE <b style="color: rgb(245,223,56)">MUUCHXIIMBAL</b>?</h1>
+            <h3 style="color: rgb(58,58,58);"><b style="color: rgb(245,223,56)">MUUCHXIIMBAL</b> te da el respaldo y seguridad que necesitas para lograr tus objetivos. </h3>
+            <p>Después de años de experiencia práctica (tanto personal como laboral), nuestro equipo de profesionales apasionados es su única opción!</p>
 
+            <div class="container2">
+              <a class="btn btn-1">Saber mas de MUUCHXIIMBAL</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+<article class="banner2">
+  <p>Consultora técnico legal ambiental formada por profesionales altamente calificados</p>
+  <div class="container3">
+    <a class="btn btn-1">Ir a Servicios</a>
+  </div>
+</article>
+
+<div style="margin-top: 2%; margin-bottom: 2%;" class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h1 class="text-center" style="color: rgb(215,14,22);">Algunos de nuestros servicios</h1>
+            <p class="text-center" style="color: rgb(19,41,58);">Impulsamos el desarrollo sustentable</p>
+          </div>
+        </div>
+        <div class="row" style="margin-top: 2%;">
+          <div class="col-md-2">
+            <img src="img/estudios2.jpg"
+            class="img-rounded img-responsive">
+          </div>
+          <div class="col-md-4">
+            <h3 class="text-left">ESTUDIOS DE RIESGO AMBIENTAL</h3>
+            <p class="text-left">Las actividades donde se manejan o utilizan sustancias peligrosas, requieren la presentación de un Estudio de Riesgo Ambiental.</p>
+          </div>
+          <div class="col-md-2">
+            <img src="img/servicios.jpg"
+            class="img-rounded img-responsive">
+          </div>
+          <div class="col-md-4">
+            <h3 class="text-left">SERVICIOS AMBIENTALES</h3>
+            <p class="text-left">* Programas de mejores práticas de manejo. <br>
+                                  * Gestión de trámites y apoyos ante dependencias. <br>
+                                  * Cursos de capacitación.
+            </p>
+          </div>
+        </div>
+        <div class="row" style="margin-top: 2%;">
+          <div class="col-md-2">
+            <img src="img/ingenieria2.jpg"
+            class="img-rounded img-responsive">
+          </div>
+          <div class="col-md-4">
+            <h3 class="text-left">Ingeniería Ambiental</h3>
+            <p class="text-left">Estudio de los problemas ambientales sin descuidar las cuestiones vinculadas a la ecología, la economía y los temas sociales, siempre con un enfoque de sostenibilidad para las generaciones futuras.</p>
+          </div>
+          <div class="col-md-2">
+            <img src="img/auditoria.png"
+            class="img-rounded img-responsive">
+          </div>
+          <div class="col-md-4 text-center">
+            <h3 class="text-left">Auditorias Ambientales</h3>
+            <p class="text-left">Asesorar a las empresas en la implantación de un Sistema de gestión Ambiental. Tener un sistema de gestión ambiental certificado supone para la empresa una serie de beneficios de mercado, económicos, de mejora de la imagen.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+<?php include 'vistas/pie.php' ?>
 <script src="js/jquery.js"></script>
 <script src="js/bootstrap.js"></script>
 <script src="js/menu.js"></script>
