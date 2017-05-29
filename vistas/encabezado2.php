06fe94a0c6aa0c6e40667bbfcee4beaf6ad6ac90
esteban1234 <tva_libra@hotmail.com>
2017-05-29 17:23:47 -0500

    metas

--- vistas/encabezado2.php
+++ vistas/encabezado2.php
@@ -8,8 +8,8 @@
 	 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
   	<META NAME="Title" content="MUUCHXIIMBAL">
   	<META NAME="Author" CONTENT="[]"/>
-  	<META name="Description" content="Somos una compañía ubicada en la ciudad de mérida, yucatan, dedicados a ofrecer servicios de consultoría en medio ambiente, Servicios de levantamiento geofísico, Servicios de arquitectura."/>
-  	<META NAME="Keywords" CONTENT="consultoría en medio ambiente en mérida, servicios de levantamiento geofísico en mérida, consultoría en medio ambiente, consultoría en medio ambiente en mérida yucatan, servicios de levantamiento geofísico en merida yucatan, servicios de levantamiento geofísico, servicios de arquitectura."/>
+  	<META name="Description" content="Somos una compañía ubicada en la ciudad de mexico, dedicados a ofrecer servicios de consultoría en medio ambiente, Servicios de levantamiento, arquitectura, Servicios de arquitectura."/>
+  	<META NAME="Keywords" CONTENT="consultoría en medio ambiente en mexico, servicios de levantamiento arquitectonico en mexico, consultoría en medio ambiente, consultoría en medio ambiente en mexico , servicios de levantamiento,servicios de arquitectura."/>
   	<link rel="stylesheet" href="../css/bootstrap.css">
   	<link rel="stylesheet" href="../css/iconos.min.css">
 	<link rel="stylesheet" href="../css/estilos.css">
